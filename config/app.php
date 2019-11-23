749abaf61379d006c9b3953fe74d9b2daf9ae511
Bryan Bojorque <bryan@di.net.au>
2019-11-23 12:11:58 +0800

    final commit before heroku deploy

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Bom Management'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://cb238f89.ngrok.io'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
